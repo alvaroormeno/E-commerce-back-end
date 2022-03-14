d7c1bd1db9029cab580bbc9dd5814d5dfe53255f
alvaroormeno <alvaro@ormeno.org>
2022-03-14 00:05:31 -0400

    routes complete

--- models/index.js
+++ models/index.js
@@ -9,10 +9,12 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
+
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -20,7 +22,16 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 })
 
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
+
+
+
+
 
 module.exports = {
   Product,
